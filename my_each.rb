4100abd547445acdf7c2963242cbd348db493260
Reynelle <Rtalford13@gmail.com>
2020-12-17 09:43:32 -0500

    commit tree 63107060b947982057c32a44ea6f4fa2e0c498f6

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
 def my_each # put argument(s) here
   # code here
 end
+
+def my_each(array)
+  i = 0 
+  each = []
+  while i < array.length
+  yield(array[i])
+  i += 1
+  end 
+  array
+
+end 
+
+
